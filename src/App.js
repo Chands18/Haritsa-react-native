bbff42e295ddf6db11daf1f68649541b33949c1f
Muhamad Baydhowi <muhamadbaydhowi@outlook.com>
2021-01-26 21:14:00 +0700

    Initial library setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+// TODO: Navigation container
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Main App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
