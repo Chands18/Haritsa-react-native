a13a9aec4426e7a0e8c0e077b3acff6868f74f58
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-04 15:25:14 +0700

    Fix Image Katalog Card

--- src/components/Molecules/KatalogCards/index.js
+++ src/components/Molecules/KatalogCards/index.js
@@ -1,69 +1,110 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Text, View, Image} from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList
+} from 'react-native';
 import Gap from '../../Atoms/Gap';
 
 const data = [
   {
     id: 1,
-    name: 'Product',
-    price: 'Rp.1000'
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-    price: 'Rp.1000'
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
   {
     id: 1,
-    name: 'Product',
-
+    name: 'Nama Product',
+    price: 'Rp.1000',
   },
 ];
+const renderItem=(item,index) => {
+  console.log (item)
+  return (
+    <View>
+      <View
+        style={{
+          marginTop: 10,
+          borderColor:'black',
+          width:180,
+          paddingHorizontal:10,
+          paddingTop:10,
+          marginLeft:6
+          
+        }}>
+        <Image
+          source={{
+            uri:
+              'https://i0.wp.com/www.lenterabisnis.com/wp-content/uploads/2018/07/atribut-produk.jpg?fit=600%2C400&ssl=1',
+          }}
+          style={{width: '100%', height: 170}}
+        />
+        <View style={{padding:10}}>
+          <Text>Nama Produk</Text>
+          <Text>Rp 1.000</Text>
+          <Image
+            source={require('../../../assets/Icon/logo1.png')}
+            style={{
+              width: 20,
+              height: 25,
+              position: 'absolute',
+              right: 10,
+              top: 20,
+            }}
+          />
+        </View>
+      </View>
+      <Gap width={10} />
+    </View>
+  );
+}
 const KatalogCards = () => {
   return (
-    <View style={{flexDirection: 'row', padding: 15, flexWrap: 'wrap', alignItems:'center', justifyContent:'center', marginRight: -10}}>
-        {data.map((item, index) => {
-            return (
-                <>
-                        <View style={{ marginTop:10,backgroundColor:'yellow',width:Dimensions.get('window').width/2 - 30, paddingVertical: 10}}>
-                            <Image source={{uri: "https://i0.wp.com/www.lenterabisnis.com/wp-content/uploads/2018/07/atribut-produk.jpg?fit=600%2C400&ssl=1"}} style={{width:'100%', height: 170}} />
-                            <View style={{padding:10,}}>
-                            <Text>{item.name}</Text>
-                            <Text>{item.price}</Text>
-                            <Image source={require('../../../assets/Icon/logo1.png')} style={{width:20, height: 25, position:'absolute', right:10, top:20}} />
-                            </View>
-                        </View>
-                        <Gap width={10}/>
-                        
-                        </>
-            )
-        })}
-
+    <View
+      style={{
+        backgroundColor: 'white',
+        marginTop: 20,
+        flexDirection: 'column',
+        padding: 15,
+        flexWrap: 'wrap',
+        marginRight: -10,
+      }}>
+      <Text style={{fontWeight:'bold'}}>Produk</Text>
+      <FlatList numColumns={2} renderItem={renderItem} data={data} keyExtractor={(item,index)=>index.toString()} />
+        
     </View>
   );
 };
