d5c293cf6376e074ca1ff388c134b1397c7af963
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-05 23:48:09 +0700

    Merapihkan style Button

--- src/components/Atoms/Button/index.js
+++ src/components/Atoms/Button/index.js
@@ -1,25 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image} from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Button = () => {
-    return (
-        <View style={{flexDirection:'row', width:300}}>
-            <View>
-            <TouchableOpacity style={{width:205, backgroundColor:'coral', height:40,}}>
-            <Text style={{flex:1, textAlignVertical:'center', textAlign:'center'}}>Beli Sekarang</Text>
-            </TouchableOpacity>
-            </View>
-            <View>
-            <TouchableOpacity style={{width:207 , backgroundColor:'red', height: 40}}>
-                <Text style={{color:'white', textAlign:'center', top:8}}>Keranjang</Text>
-                <Image style={{left:150, bottom:15}} source={require('./../../../assets/Icon/logo2.png')}/>
-            </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <View>
+        <TouchableOpacity style={styles.ButtonLeft}>
+          <Text style={styles.TextLeft}>Beli Sekarang</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.ButtonRight}>
+          <Text style={styles.TextRight}>Keranjang</Text>
+          <Image
+            style={styles.image}
+            source={require('./../../../assets/Icon/logo2.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-export default Button
+export default Button;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {flexDirection: 'row', width: 300},
+  ButtonLeft: {width: 205, backgroundColor: 'coral', height: 40},
+  TextLeft: {flex: 1, textAlignVertical: 'center', textAlign: 'center'},
+  ButtonRight: {width: 207, backgroundColor: 'red', height: 40},
+  TextRight: {color: 'white', textAlign: 'center', top: 8},
+  image: {left: 150, bottom: 15, width: 19, height: 25},
+});
