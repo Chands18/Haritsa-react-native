e637eb06b23f2763645f4b1744e8f5451d14d4c4
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-13 13:46:19 +0700

    add search data

--- src/components/Molecules/TransactionContent/index.js
+++ src/components/Molecules/TransactionContent/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Image} from 'react-native'
+import { icons } from '../../../assets'
 
 const TransactionContent = () => {
     return (
-        <View>
-            <Text>voila</Text>
-        </View>
+        <View style={{flexDirection:'row', paddingTop:10,marginBottom:10, marginHorizontal:20,width:'100%'}}>
+                <View style={{position:'relative', flex:1,}}>
+                <TextInput placeholder="Search Data" style={{elevation:10 ,borderRadius:10,height:40,fontSize:13,backgroundColor:'white', textAlign:'center'}}/>
+                <Image source={icons.ic_search} style={{position:'absolute',width:17,height:17, top:10, right:20}}/>
+                </View>
+                <View style={{width:35,alignItems:'center',justifyContent:'center',bottom:0}}>  
+                </View>
+            </View>
     )
 }
 
