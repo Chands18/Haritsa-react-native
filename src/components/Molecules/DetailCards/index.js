9a3ed97cdce193314931429ecfe06168e90ff0a8
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-13 13:05:35 +0700

    navigation detailcards

--- src/components/Molecules/DetailCards/index.js
+++ src/components/Molecules/DetailCards/index.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import {
   Dimensions,
@@ -5,40 +6,48 @@ import {
   Text,
   View,
   Image,
-  FlatList
+  FlatList,
 } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { dummiesProduct, icons } from '../../../assets';
-import { FormatCurrency } from '../../../utils';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {dummiesProduct, icons} from '../../../assets';
+import {FormatCurrency} from '../../../utils';
 import Gap from '../../Atoms/Gap';
 
-
-const renderItem=(item,index) => {
-  console.log (item)
-  return (
-    <View>
+const DetailCards = () => {
+  const navigation = useNavigation();
+  const renderItem = (item, index) => {
+    console.log(item);
+    return (
+      <View>
         <View
           style={{
             marginTop: 10,
-            width:180,
-            paddingHorizontal:10,
-            paddingTop:10,
-            marginLeft:6,
-            borderWidth:1,
-            borderRadius:10,
-            borderColor:'lightgrey',
+            width: 180,
+            paddingHorizontal: 10,
+            paddingTop: 10,
+            marginLeft: 6,
+            borderWidth: 1,
+            borderRadius: 10,
+            borderColor: 'lightgrey',
           }}>
-          <Image
-            source={item.item.image}
-            style={{width: '100%', height: 170,}}
-          />
-          <View style={{padding:10, backgroundColor:'white'}}>
+          <Image source={item.item.image} style={{width: '100%', height: 170}} />
+          <View style={{padding: 10, backgroundColor: 'white'}}>
             <Text>{item.item.name}</Text>
-            <Text>{FormatCurrency({num:item.item.price})}</Text>
+            <Text>{FormatCurrency({num: item.item.price})}</Text>
           </View>
           <View>
-            <TouchableOpacity>
-              <View style={{padding:10,elevation:0,width:150,alignSelf:'center', backgroundColor:'deeppink', borderRadius:10, flexDirection:'row', justifyContent:'space-evenly'}}>
+            <TouchableOpacity onPress={()=>navigation.navigate('Keranjang')}>
+              <View
+                style={{
+                  padding: 10,
+                  elevation: 0,
+                  width: 150,
+                  alignSelf: 'center',
+                  backgroundColor: 'deeppink',
+                  borderRadius: 10,
+                  flexDirection: 'row',
+                  justifyContent: 'space-evenly',
+                }}>
                 <Image
                   source={icons.ic_logo}
                   style={{
@@ -46,16 +55,17 @@ const renderItem=(item,index) => {
                     height: 25,
                   }}
                 />
-                  <Text style={{fontWeight:'bold', color:'white',}}>Add to Cart</Text>
+                <Text style={{fontWeight: 'bold', color: 'white'}}>
+                  Add to Cart
+                </Text>
               </View>
-              </TouchableOpacity>
-              </View>    
+            </TouchableOpacity>
+          </View>
         </View>
         <Gap width={10} />
-    </View>
-  );
-}
-const DetailCards = () => {
+      </View>
+    );
+  };
   return (
     <View
       style={{
@@ -65,9 +75,13 @@ const DetailCards = () => {
         padding: 15,
         flexWrap: 'wrap',
       }}>
-      <Text style={{fontWeight:'bold'}}>Related Products</Text>
-      <FlatList numColumns={2} renderItem={renderItem} data={dummiesProduct} keyExtractor={(item,index)=>index.toString()} />
-        
+      <Text style={{fontWeight: 'bold'}}>Related Products</Text>
+      <FlatList
+        numColumns={2}
+        renderItem={renderItem}
+        data={dummiesProduct}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 };
