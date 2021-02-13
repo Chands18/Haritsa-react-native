d48e81d0e5fafcf92802fa3519444d77a84199ab
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-13 13:03:06 +0700

    header detail news

--- src/components/Molecules/HeaderDetailNews/index.js
+++ src/components/Molecules/HeaderDetailNews/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
 const HeaderDetailNews = () => {
     return (
-        <View>
-            <Text>test</Text>
+        <View style={{backgroundColor:'deeppink',height:60, flexDirection:'row', justifyContent:'flex-start', alignItems:'center', width:'100%'}}> 
+            <Image source={require('../../../assets/Icon/Vector.png')}/>
+            <Text style={{fontSize:18,fontWeight:'bold', textAlign:'center',left:15, color:'white' }}>News Title</Text>
         </View>
     )
 }
