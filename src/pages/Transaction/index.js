5df165f27701d5c7b7d928cd5efb639feac291d3
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-15 19:15:47 +0700

    styling page transaksi

--- src/pages/Transaction/index.js
+++ src/pages/Transaction/index.js
@@ -4,7 +4,7 @@ import { HeaderTransaction, TransactionContent } from '../../components'
 
 const Transaction = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <HeaderTransaction/>
             <TransactionContent/>
         </View>
@@ -13,4 +13,9 @@ const Transaction = () => {
 
 export default Transaction
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'white',
+        height:'100%'
+    }
+})
