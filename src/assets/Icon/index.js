1215b2b2fe070a05fe3d3b8fc36fc6a951221f56
Faridz Kurnia Chandra <faridzkurniaa@gmail.com>
2021-02-17 14:23:18 +0700

    change icon

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -1,8 +1,12 @@
 export const icons={
     ic_backicon:require('./BackIcon.png'),
     ic_logo:require('./hrtsicon.png'),
-    ic_cart:require('./Vector1.png'),
+    ic_cart:require('./cart2.png'),
     ic_search:require('./Vector2.png'),
     ic_line:require('./Line.png'),
-    ic_download:require('./down-arrow.png')
+    ic_download:require('./down-arrow.png'),
+    ic_home:require('./home.png'),
+    ic_transaction:require('./transaction2.png'),
+    ic_brosur:require('./brosur2.png'),
+    ic_akun:require('./akun.png')
 }
